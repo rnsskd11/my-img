ee29733bece7ab33b2895576d8af5cfff4d4988e
rnsskd11 <rnsskd11@naver.com>
2020-11-02 04:44:14 +0900

    2020-11-02

--- magazine_b/js/script.js
+++ magazine_b/js/script.js
@@ -1,39 +1,69 @@
-$(document).ready(function(){
-    
-    $(".search").click(function(){
-        if($(".search-on").css("display")==("none")){
-            $(".search-on").css({"display":"block"})
-        } 
-        
-    })
-    $(".main-page").click(function(){
-        if($(".search-on").css("display")==("block")){
-            $(".search-on").css({'display':'none'})
-        }
-    })
-    $(".bg-F").mouseover(function(){
-        $(".bg-B").css({'left':'-85%'})
-    })
-    $(".bg-F").mouseleave(function(){
-        $(".bg-B").css({'left':'0px'})
-    })
-    $(".bg-J").mouseover(function(){
-        $(".bg-B").css({'left':'-85%'});
-        $(".bg-F").css({'left':'-85%'})
-    })
-    $(".bg-J").mouseleave(function(){
-        $(".bg-B").css({'left':'0px'})
-        $(".bg-F").css({'left':'0px'})
-    })
+$(document).ready(function () {
 
-    // --------슬릭 슬라이더---------
-    $('.shop-slider').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
-      });
+    function Search__init() {
+        $(".search").click(function () {
+            if ($(".search-on").css("display") == ("none")) {
+                $(".search-on").css({
+                    "display": "block"
+                })
+            }
+        })
+    }
+    Search__init();
 
+    function Section2__init() {
+        $(".main-page").click(function () {
+            if ($(".search-on").css("display") == ("block")) {
+                $(".search-on").css({
+                    'display': 'none'
+                })
+            }
+        })
+        $(".bg-F").mouseover(function () {
+            $(".bg-B").css({
+                'left': '-85%'
+            })
+        })
+        $(".bg-F").mouseleave(function () {
+            $(".bg-B").css({
+                'left': '0px'
+            })
+        })
+        $(".bg-J").mouseover(function () {
+            $(".bg-B").css({
+                'left': '-85%'
+            });
+            $(".bg-F").css({
+                'left': '-85%'
+            })
+        })
+        $(".bg-J").mouseleave(function () {
+            $(".bg-B").css({
+                'left': '0px'
+            })
+            $(".bg-F").css({
+                'left': '0px'
+            })
+        })
+    }
+    Section2__init();
 
+    function Section3__init() {
+
+        $(".shop-slider").slick({
+            infinite: false,
+            slidesToShow: 3,
+            slidesToScroll: 3
+        });
+
+    }
+    Section3__init();
+    function Section4__init(){
+        $('.b-cast-slider').slick({
+            infinite: false,
+        });
+    }
+    Section4__init();
 
 
 })
