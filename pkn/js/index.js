65b11d6fbf62bbfa379b79f28c040aad2b590e22
rnsskd11 <rnsskd11@naver.com>
2022-02-09 01:16:11 +0900

    2022-02-09

---
+++ pkn/js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    function PjList__init() {
+
+        $('.pj_hover>a').click(function(){
+            $('.pj_list').toggleClass('active')
+        })
+    }
+    PjList__init();
+
+    // function Menu__init() {
+    //     $('.menu-i').click(function () {
+    //       $('.menu-i,#menu-on').toggleClass('active')
+    //     })
+    //   }
+    //   Menu__init();
+
+
+
+})
+
+
